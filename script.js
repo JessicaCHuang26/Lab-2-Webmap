40776232117c3d6eace6a462aeac21b283f6df0f
Jessica Huang <jessicachieh.huang@mail.utoronto.ca>
2026-02-10 16:53:04 -0500

    updated comment

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-// add Mapbox token
+//add Mapbox token
 mapboxgl.accessToken =
   "pk.eyJ1IjoiamVzc2ljYWh1YW5nIiwiYSI6ImNtazNjNmdmeTBkN3AzZnEyZHRscHdod28ifQ.Pa9LhzBk1H75KBMwBngDjA";
 
-// create map container
+// create map view
 const map = new mapboxgl.Map({
-  container: "my-map", // map container ID
-  style: "mapbox://styles/jessicahuang/cmle4jgcc00dv01qqd84hg94o", // add my own style URL
-  center: [-79.38718, 43.658], // starting position [lng, lat]
-  zoom: 13, // starting zoom level,
-  pitch: 15, // tilt (0 = flat, 60 = nice 3D),
-  bearing: -15, // rotate map to upright position
+  container: "my-map",
+  style: "mapbox://styles/jessicahuang/cmle4jgcc00dv01qqd84hg94o", //add my own style URL
+  center: [-79.38718, 43.658], //set starting position
+  zoom: 13,
+  pitch: 15, //tilt the map view
+  bearing: -15, //rotate map to upright position
 });
 
 map.on("load", () => {
-  // Add cafe feature data source
+  //Add cafe feature data source
   map.addSource("cafe-data", {
     type: "geojson",
     data: "https://raw.githubusercontent.com/JessicaCHuang26/Lab-2-Webmap/main/data/cafe-point.geojson",
   });
 
-  // Load image for cafe feature symbology
+  //Load image(coffe cup) for cafe feature symbology
   map.loadImage(
     "https://raw.githubusercontent.com/JessicaCHuang26/Lab-2-Webmap/main/data/coffee.webp",
     (error, image) => {
       if (error) throw error;
 
-      map.addImage("coffee", image); // add image to map
+      map.addImage("coffee", image); //add image to map
 
-      //add cafe feature and its symbology to map
+      //add cafe feature to map
       map.addLayer({
         id: "cafe-point",
         type: "symbol",
@@ -35,52 +35,41 @@ map.on("load", () => {
         layout: {
           "icon-image": "coffee",
           "icon-size": 0.15,
-          "icon-allow-overlap": true, // show all icon to ensure no hidden icon when overlapped
-          "icon-ignore-placement": true,
+          "icon-allow-overlap": true, //allow icon to overlap
+          "icon-ignore-placement": true, //make icon visible even if they collides with each other
         },
       });
     },
   );
 
-  // Visualize data layer on map
-  // map.addLayer({
-  //   id: "cafe-point",
-  //   type: "circle",
-  //   source: "cafe-data",
-  //   paint: {
-  //     "circle-radius": 3,
-  //     "circle-color": "#ffffff",
-  //     "circle-stroke-color": "#38429e",
-  //     "circle-stroke-width": 3,
-  //   },
-  // });
-
+  //add name label to cafés
   map.addLayer({
     id: "cafe-label",
     type: "symbol",
     source: "cafe-data",
     layout: {
-      "text-field": ["get", "Name"], // <-- column name from GeoJSON
+      "text-field": ["get", "Name"], //get cafe name from GeoJSON
       "text-size": 14,
-      "text-offset": [0, 1.2], // move text above the dot
-      "text-anchor": "top",
+      "text-offset": [0, 1.2],
+      "text-anchor": "top", //move text above the icon
       "text-font": ["Open Sans Bold"],
-      "text-allow-overlap": true, // show all labels
+      "text-allow-overlap": true, //allow label to overlap and show all labels even if they collide
       "text-ignore-placement": true,
     },
     paint: {
       "text-color": "rgb(53, 53, 173)",
-      "text-halo-color": "#ffffff", //
+      "text-halo-color": "#ffffff",
       "text-halo-width": 1,
     },
   });
 
+  //implement clicking interaction to cafe features
   map.on("click", "cafe-point", (e) => {
     const feature = e.features[0];
-    const coords = feature.geometry.coordinates.slice(); // slice avoids mutation bug
+    const coords = feature.geometry.coordinates.slice();
     const props = feature.properties;
 
-    // fly to cafe first
+    //once clicked, zoom/fly map view to the cafe
     map.flyTo({
       center: coords,
       zoom: 17,
@@ -90,7 +79,7 @@ map.on("load", () => {
       essential: true,
     });
 
-    // wait until fly animation finishes
+    //once the flyTo interaction end, add address pop-up for the café
     map.once("moveend", () => {
       new mapboxgl.Popup({ offset: [0, -25] })
         .setLngLat(coords)
@@ -99,11 +88,13 @@ map.on("load", () => {
     });
   });
 
+  //add TTC data
   map.addSource("subway-data", {
     type: "geojson",
     data: "https://raw.githubusercontent.com/JessicaCHuang26/Lab-2-Webmap/main/data/TTC-Near_Campus.geojson",
   });
-  // Visualize data layer on map
+
+  //visualize TTC data layer on map
   map.addLayer({
     id: "subway-point",
     type: "circle",
@@ -116,14 +107,15 @@ map.on("load", () => {
     },
   });
 
+  //add TTC name label for each station
   map.addLayer({
     id: "subway-label",
     type: "symbol",
     source: "subway-data",
     layout: {
-      "text-field": ["get", "TTC Station"], // <-- column name from GeoJSON
+      "text-field": ["get", "TTC Station"], //get name of stations from GeoJSON in 'TTC Station' column
       "text-size": 12,
-      "text-offset": [1, 0], // move text above the dot
+      "text-offset": [1, 0], //move text above the circle marker
       "text-anchor": "top",
     },
     paint: {
@@ -132,12 +124,13 @@ map.on("load", () => {
       "text-halo-width": 1,
     },
   });
-
+  //add café route data
   map.addSource("cafe-route-data", {
     type: "geojson",
     data: "https://raw.githubusercontent.com/JessicaCHuang26/Lab-2-Webmap/main/data/cafe-route.geojson",
   });
-  // Visualize data layer on map
+
+  //visualize café route layer on map
   map.addLayer({
     id: "cafe-route",
     type: "line",
